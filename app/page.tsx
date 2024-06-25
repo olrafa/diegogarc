3cdb804d80e1b3092b5a9d8c926a530e78320195
Rafael Olivares <olrafael@mailfence.com>
2024-06-25 10:19:42 -0300

    revert to fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,13 @@ import Image from "next/image";
 import homeImages from "@/constants/homeImages";
 
 export default function Home() {
-  const getRandomImage = () =>
-    homeImages[Math.floor(Math.random() * homeImages.length)];
-
-  const coverImage = getRandomImage();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative font-bold z-[-1] flex place-items-center before:absolute text-3xl before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#49a137] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         Diego Garc
       </div>
       <Image
-        src={coverImage}
+        src={homeImages[12]}
         alt="Site em construção"
         width={400}
         height={400}
