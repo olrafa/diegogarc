dc36164d99b22973a649a12dbac5c8da898278a1
Rafael Olivares <olrafael@mailfence.com>
2024-06-24 21:33:01 -0300

    first commit

---
+++ constants/homeImages.ts
@@ -0,0 +1,37 @@
+import iching1 from "@/assets/images/45_iching-1.jpg";
+import iching2 from "@/assets/images/45_iching-2.jpg";
+import iching3 from "@/assets/images/45_iching-3.jpg";
+import iching4 from "@/assets/images/45_iching-4.jpg";
+import iching5 from "@/assets/images/45_iching-5.jpg";
+import iching6 from "@/assets/images/45_iching-6.jpg";
+import iching7 from "@/assets/images/45_iching-7.jpg";
+import iching8 from "@/assets/images/45_iching-8.jpg";
+import iching9 from "@/assets/images/45_iching-9.jpg";
+import iching10 from "@/assets/images/45_iching-10.jpg";
+import iching11 from "@/assets/images/45_iching-11.jpg";
+import iching12 from "@/assets/images/45_iching-12.jpg";
+import iching13 from "@/assets/images/45_iching-13.jpg";
+import iching14 from "@/assets/images/45_iching-14.jpg";
+import iching15 from "@/assets/images/45_iching-15.jpg";
+import iching16 from "@/assets/images/45_iching-16.jpg";
+
+const homeImages = [
+  iching1,
+  iching2,
+  iching3,
+  iching4,
+  iching5,
+  iching6,
+  iching7,
+  iching8,
+  iching9,
+  iching10,
+  iching11,
+  iching12,
+  iching13,
+  iching14,
+  iching15,
+  iching16,
+];
+
+export default homeImages;
