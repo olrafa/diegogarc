cdbc7158f3bcee54f8ce167efd89d3728977d6f5
Rafael Olivares <olrafael@mailfence.com>
2024-11-03 13:01:45 -0300

    nav

---
+++ components/navigation/LinkList.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const LinkList = () => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    return pathname === path ? "font-bold underline" : "text-mainText";
+  };
+
+  const linkList = [
+    {
+      href: "/",
+      text: "Reel",
+    },
+    {
+      href: "/direcao-de-fotografia",
+      text: "Direção de fotografia",
+    },
+    {
+      href: "/operacao-de-camera",
+      text: "Operação de câmera",
+    },
+    {
+      href: "/bio",
+      text: "Bio",
+    },
+    {
+      href: "/contato",
+      text: "Contato",
+    },
+  ];
+
+  return (
+    <ul className="flex flex-col gap-4 uppercase">
+      {linkList.map(({ href, text }) => (
+        <li key={href} className={isActive(href)}>
+          <Link href={href}>{text}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LinkList;
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import DiegoIcon from "./DiegoIcon";
+import LinkList from "./LinkList";
 
 const Navigation = () => (
   <aside className="w-1/4 flex flex-col h-screen p-12">
@@ -7,23 +8,7 @@ const Navigation = () => (
       <Link href="/">
         <DiegoIcon />
       </Link>
-      <ul className="flex flex-col gap-4 uppercase">
-        <li>
-          <Link href="/">Reel</Link>
-        </li>
-        <li>
-          <Link href="/direcao-de-fotografia">Direção de fotografia</Link>
-        </li>
-        <li>
-          <Link href="/operacao-de-camera">Operação de câmera</Link>
-        </li>
-        <li>
-          <Link href="/bio">Bio</Link>
-        </li>
-        <li>
-          <Link href="/contato">Contato</Link>
-        </li>
-      </ul>
+      <LinkList />
     </nav>
   </aside>
 );
