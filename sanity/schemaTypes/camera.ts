da2e26b0023cd7d80830b030bfe859c64c753750
Rafael Olivares <olrafael@mailfence.com>
2024-11-03 13:40:30 -0300

    mo types

---
+++ sanity/schemaTypes/camera.ts
@@ -0,0 +1,32 @@
+import { defineType } from "sanity";
+
+export const cameraType = defineType({
+  name: "camera",
+  title: "Operação de câmera",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Título",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Descrição",
+      type: "string",
+    },
+    {
+      name: "order",
+      title: "Ordem",
+      description: "Em qual ordem aparece na lista de trabalhos",
+      type: "number",
+    },
+    { name: "poster", title: "Pôster", type: "image" },
+    {
+      name: "page",
+      title: "Página",
+      type: "reference",
+      to: [{ type: "cinematography" }],
+    },
+  ],
+});
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { reelType } from "./reel";
+import { photographyType } from "./fotografia";
+import { cameraType } from "./camera";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [reelType],
+  types: [reelType, photographyType, cameraType],
 };
