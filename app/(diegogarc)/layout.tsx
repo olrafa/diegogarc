2eeef395958c62f9956078a97c5451f04f5b83ef
Rafael Olivares <olrafael@mailfence.com>
2024-11-03 10:38:45 -0300

    layout

--- app/(diegogarc)/layout.tsx
+++ app/(diegogarc)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/Header";
 
 const noto = Noto_Sans({ subsets: ["latin"] });
