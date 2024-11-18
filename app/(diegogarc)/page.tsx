1523414fcb10689cb1508ead20daa5725e454127
Rafael Olivares <olrafael@mailfence.com>
2024-11-18 20:01:39 -0300

    async home

--- app/(diegogarc)/page.tsx
+++ app/(diegogarc)/page.tsx
@@ -1,9 +1,28 @@
-import VideoMain from "@/components/Reel";
+import { VIMEO } from "@/constants/links";
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
 
-export default function Home() {
+export default async function Home() {
+  const latestReel = groq`*[_type == "reel"] | order(year desc)[0]`;
+  const { video } = await client.fetch(latestReel);
+
+  const videoSrc = video.replace(
+    "https://vimeo.com/",
+    "https://player.vimeo.com/video/"
+  );
+
   return (
     <main className="w-full flex-col items-center justify-between lg:p-4 lg:mt-12">
-      <VideoMain />
+      <div className="w-full max-w-7xl flex justify-center px-2">
+        <iframe
+          title={VIMEO.title}
+          className="w-full aspect-video rounded"
+          src={videoSrc}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share: fullscreen"
+          loading="lazy"
+          allowFullScreen
+        />
+      </div>
     </main>
   );
 }
